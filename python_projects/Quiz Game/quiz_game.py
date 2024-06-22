a5828d25db32f2a7989ffaddfe19d051690efc1b
adnan-bin-wahid <adnan.iit.du@gmail.com>
2024-06-22 16:11:48 +0600

    Quiz Game Added

--- python_projects/Quiz Game/quiz_game.py
+++ python_projects/Quiz Game/quiz_game.py
@@ -1,1 +1,43 @@
 print("Welcome to my computer quiz !")
+
+playing = input("Do you want to play? ")
+
+if(playing.lower() != "yes"):
+    print("quitting the game !")
+    quit()
+else:
+    print("Okay! Let's play :)")
+score =0
+    
+    
+
+
+answer = input("What does GPU stand for? ")
+
+if (answer.lower() == "graphics processing unit"):
+    print("correct ! ")
+    score += 1
+else: 
+    print("Incorrect ! ")
+
+
+
+answer = input("What does RAM stand for? ")
+
+if (answer.lower() == "random access memory"):
+    print("correct ! ")
+    score += 1
+else: 
+    print("Incorrect ! ")
+
+
+answer = input("What does PSU stand for? ")
+
+if (answer.lower() == "power supply"):
+    print("correct ! ")
+    score += 1
+else: 
+    print("Incorrect ! ")
+    
+print("You Got "+str(score)+" questions correct !")
+print("You Got "+str((score/3)*100)+"%")
