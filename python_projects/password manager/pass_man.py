8acafe3e86becdf8bcb06daa08b8a52984a85de9
adnan-bin-wahid <adnan.iit.du@gmail.com>
2024-07-14 20:07:12 +0600

    modified

--- python_projects/password manager/pass_man.py
+++ python_projects/password manager/pass_man.py
@@ -1,14 +1,18 @@
 master_pass = input("What is the master password: ")
 
 def view():
-    pass
+    with open("pass_man.txt","r") as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, password = data.split("|")
+            print("username:",user,", password:",password)
     
 
 def add():
     with open("pass_man.txt","a") as f: 
         user_name = input("Enter your user name: ")
-        password = input("Enter your password")
-        f.write(user_name+"|"+password)
+        password = input("Enter your password: ")
+        f.write(user_name+"|"+password + "\n")
 
 
 
